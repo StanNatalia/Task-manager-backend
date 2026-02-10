bfc0d9271065c46711868a2f71c41d87118230c1
Natalia Stan <popkids.ua@gmail.com>
2026-02-10 18:24:05 +0200

    chore: remove dist from repository

--- dist/utils/generateId.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = void 0;
-const generateId = () => Math.random().toString(36).substring(2, 10);
-exports.generateId = generateId;
-//# sourceMappingURL=generateId.js.map
